9a88f7ac875daae18019f18211010f6cd316ad0b
jcttqh <44391287+jcttqh@users.noreply.github.com>
2019-12-29 23:32:34 +0800

    update

--- homepage.php
+++ homepage.php
@@ -1,43 +1,211 @@
-<!DOCTYPE html>
+<?php
+?>
 <html>
     <head>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
               integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <link rel="stylesheet" href="my.css">
+        <link rel="stylesheet" href="CSS/Home/home.css">
+        <link href="https://fonts.googleapis.com/css?family=Cinzel+Decorative|Crimson+Text&display=swap" rel="stylesheet">
+
     </head>
-    <body class="module" style="background:url(Resource/Photo/jct/0.jpg);  background-size:cover; background-repeat: no-repeat;" >
-        <div class="pos-f-t">
-            <nav class="navbar navbar-dark">
-                <div class="btn-group">
-                    <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" data-display="static" aria-haspopup="true" aria-expanded="false" style="background-color:#A8D8B9;">
-                        <span class="navbar-toggler-icon" style="height: 30px; width: 30px;"></span>
-                    </button>
-                    <div class="dropdown-menu dropdown-menu-left">
-                        <button class="dropdown-item" type="button">Homepage</button>
-                        <button class="dropdown-item" type="button">Movie introduction</button>
-                        <button class="dropdown-item" type="button">Shop</button>
+    <body class="module" style="background:url(Resource/Photo/jct/18.jpeg);  background-size:cover; background-repeat: no-repeat;" >
+
+        <nav class="navbar navbar-dark">
+            <div class="btn-group">
+                <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" data-display="static" aria-haspopup="true" aria-expanded="false" style="background-color:#A8D8B9;">
+                    <span class="navbar-toggler-icon" style="height: 30px; width: 30px;"></span>
+                </button>
+                <div class="dropdown-menu dropdown-menu-left">
+                    <button class="dropdown-item" type="button">Homepage</button>
+                    <button class="dropdown-item" type="button">Movie introduction</button>
+                    <button class="dropdown-item" type="button">Shop</button>
+                </div>
+            </div>
+            <a href="#" style="margin-top: 1%;"><img src="Resource/Photo/jct/logo5.png"></a>
+            <div class="btn-group">
+                <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" data-display="static" aria-haspopup="true" aria-expanded="false" style="background-color: #A8D8B9">
+                    <span>TonyJct</span><span>&nbsp;&nbsp;</span>
+                    <img class="button" style="width: 80px;"src="Resource/Photo/jct/27.jpg">
+                </button>
+                <div class="dropdown-menu dropdown-menu-right">
+                    <button class="dropdown-item" type="button">Login</button>
+                    <button class="dropdown-item" type="button">Shopping cart</button>
+                    <button class="dropdown-item" type="button">Log out</button>
+                </div>
+            </div>
+
+        </nav>
+
+
+        <div class="container" style="height: 2800px;">  
+            <div class="texthome" style="margin-top: 10%"><p style="font-size: 64px;font-weight: bold;font-family: Cinzel Decorative"> Introduction to Miyazaki Hayao</p></div>
+            <p class="texthome3" style="font-size: 24px;font-weight: bold;font-family: Crimson Text; margin-top: 7%;">Serve heart and soul to the follower of Miyazaki Hyao</p>
+
+            <div class="arrow" style="margin-top: 15%;">
+                <a href="#down"><span></span>
+                    <span></span>
+                    <span></span></a>
+
+            </div>
+
+            <div class="row row-cols-1 row-cols-md-3" style="margin-top:35%;">
+                <div class="col mb-4" id="down">
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <img src="Resource/Photo/jct/9/000_01.jpg" alt="Responsive image" style="width:300px;height:300px;">
+                            </div>
+                            <div class="flip-card-back">
+                                <a href="https://ghibli.fandom.com/wiki/Totoro"><img src="Resource/Photo/jct/9/1.jpg" alt="Responsive image" style="width:300px;height:300px;"></a>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <a href="#" style="margin-top: 1%;"><img src="Resource/Photo/jct/logo2%20(1).png"></a>
-                <div class="btn-group">
-                    <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" data-display="static" aria-haspopup="true" aria-expanded="false" style="background-color: #A8D8B9">
-                        <span>TonyJct</span><span>&nbsp;&nbsp;</span>
-                        <img class="button" style="width: 80px;"src="Resource/Photo/jct/27.jpg">
-                    </button>
-                    <div class="dropdown-menu dropdown-menu-right">
-                        <button class="dropdown-item" type="button">Login</button>
-                        <button class="dropdown-item" type="button">Shopping cart</button>
-                        <button class="dropdown-item" type="button">Log out</button>
+                <div class="col mb-4">
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <img src="Resource/Photo/jct/9/000_02.jpg" alt="Responsive image" style="width:300px;height:300px;">
+                            </div>
+                            <div class="flip-card-back">
+                                <a href="https://ghibli.fandom.com/wiki/Howl%27s_Moving_Castle"><img src="Resource/Photo/jct/9/6.jpg" alt="Responsive image" style="width:300px;height:300px;"></a>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
-            </nav>
+                <div class="col mb-4">
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <img src="Resource/Photo/jct/9/000_03.gif" alt="Responsive image" style="width:300px;height:300px;">
+                            </div>
+                            <div class="flip-card-back">
+                                <a href="https://ghibli.fandom.com/wiki/Princess_Mononoke"><img src="Resource/Photo/jct/9/3.jpg" alt="Responsive image" style="width:300px;height:300px;"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col mb-4">
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <img src="Resource/Photo/jct/9/000_04.jpg" alt="Responsive image" style="width:300px;height:300px;">
+                            </div>
+                            <div class="flip-card-back">
+                                <a href="https://ghibli.fandom.com/wiki/Valley_of_the_Wind"><img src="Resource/Photo/jct/9/4.jpg" alt="Responsive image" style="width:300px;height:300px;"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-4">
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <img src="Resource/Photo/jct/9/000_05.jpg" alt="Responsive image" style="width:300px;height:300px;">
+                            </div>
+                            <div class="flip-card-back">
+                                <a href="https://ghibli.fandom.com/wiki/Spirited_Away"><img src="Resource/Photo/jct/9/2.jpg" alt="Responsive image" style="width:300px;height:300px;"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-4">
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <img src="Resource/Photo/jct/9/000_06.gif" alt="Responsive image" style="width:300px;height:300px;">
+                            </div>
+                            <div class="flip-card-back">
+                                <a href="https://ghibli.fandom.com/wiki/The_Wind_Rises"><img src="Resource/Photo/jct/9/7.jpg" alt="Responsive image" style="width:300px;height:300px;"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-4">
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <img src="Resource/Photo/jct/9/000_07.gif" alt="Responsive image" style="width:300px;height:300px;">
+                            </div>
+                            <div class="flip-card-back">
+                                <a href="https://ghibli.fandom.com/wiki/The_Cat_Returns"><img src="Resource/Photo/jct/9/8.jpg" alt="Responsive image" style="width:300px;height:300px;"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-4">
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <img src="Resource/Photo/jct/9/000_08.gif" alt="Responsive image" style="width:300px;height:300px;">
+                            </div>
+                            <div class="flip-card-back">
+                                <a href="https://ghibli.fandom.com/wiki/Kiki%27s_Delivery_Service"><img src="Resource/Photo/jct/9/12.jpg" alt="Responsive image" style="width:300px;height:300px;"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col mb-4">
+                    <div class="flip-card">
+                        <div class="flip-card-inner">
+                            <div class="flip-card-front">
+                                <img src="Resource/Photo/jct/9/000_09.gif" alt="Responsive image" style="width:300px;height:300px;">
+                            </div>
+                            <div class="flip-card-back">
+                                <a href="https://ghibli.fandom.com/wiki/Laputa:_Castle_in_the_Sky"><img src="Resource/Photo/jct/9/11.jpg" alt="Responsive image" style="width:300px;height:300px;"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel" style="margin-top: 10%">
+                <p style="font-size: 64px;color: white; font-weight: bold;font-family: Crimson Text; margin-top: 7%;">Future Activity</p>
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <a href="https://www.nact.jp/"><img src="Resource/Photo/jct/timg6.jpeg" class="d-block w-100" alt="..."></a>
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Totoro show</h5>
+                            <p>The National Art Center, Tokyo , 25/1/2020</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <a href="https://www.moma.org/"><img src="Resource/Photo/jct/20.jpg" class="d-block w-100" alt="..."></a>
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Spirited Away show</h5>
+                            <p>The Museum of Modern Art, New York, 20/4/2020</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <a href="http://www.artgallery.gov.my/?page_id=3263&lang=en"><img src="Resource/Photo/jct/21.jpg" class="d-block w-100" alt="..."></a>
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Hawl's Moving Castle</h5>
+                            <p>National Art Gallery, Kuala Lumpur, 15/6/2020</p>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+
+
         </div>
 
-        <div class="container" style="height: 2000px; background-color: rgba(255,255,255,.5);">
-
-        </div>
-        <hr style="border:1px dashed #0000fff"/>
 
 
 
